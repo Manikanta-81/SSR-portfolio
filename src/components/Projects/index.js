30ef3811bd99d65c3847627b794a8ddee3443bd2
Manikanta-81 <vaddimanikanta2002@gmail.com>
2025-03-10 14:19:34 +0530

    updated missing images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,60 +1,41 @@
-import React from 'react'
-import { useState } from 'react'
-import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
-import ProjectCard from '../Cards/ProjectCards'
-import { projects } from '../../data/constants'
-
+import React, { useState } from 'react';
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle';
+import ProjectCard from '../Cards/ProjectCards';
+import { projects } from '../../data/constants';
 
 const Projects = ({ openModal, setOpenModal }) => {
   const [toggle, setToggle] = useState('all');
+
   return (
     <Container id="projects">
       <Wrapper>
         <Title>Projects</Title>
         <Desc>
-        I have worked on a wide range of projects, from CAD designs to 3D modeling. Here are some of my projects using CREO, CATIA V5, and AutoCAD.
+          I have worked on a wide range of projects, from CAD designs to 3D modeling. Here are some of my projects using CREO, CATIA V5, and AutoCAD.
         </Desc>
-        <ToggleButtonGroup >
-          {toggle === 'all' ?
-            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-            :
-            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-          }
+
+        {/* Toggle Buttons for Filtering */}
+        <ToggleButtonGroup>
+          <ToggleButton active={toggle === 'all'} onClick={() => setToggle('all')}>All</ToggleButton>
           <Divider />
-          {toggle === 'CREO' ?
-            <ToggleButton active value="CREO" onClick={() => setToggle('web app')}>CREO</ToggleButton>
-            :
-            <ToggleButton value="CREO" onClick={() => setToggle('web app')}>CREO</ToggleButton>
-          }
+          <ToggleButton active={toggle === 'CREO'} onClick={() => setToggle('CREO')}>CREO</ToggleButton>
           <Divider />
-          {toggle === 'AUTO CAD' ?
-            <ToggleButton active value="AUTO CAD" onClick={() => setToggle('AUTO CAD')}>AUTO CAD</ToggleButton>
-            :
-            <ToggleButton value="AUTO CAD" onClick={() => setToggle('AUTO CAD')}>AUTO CAD</ToggleButton>
-          }
+          <ToggleButton active={toggle === 'AUTO CAD'} onClick={() => setToggle('AUTO CAD')}>AUTO CAD</ToggleButton>
           <Divider />
-          {toggle === 'CATIA - V5' ?
-            <ToggleButton active value="CATIA - V5" onClick={() => setToggle('CATIA - V5')}>CATIA - V5</ToggleButton>
-            :
-            <ToggleButton value="CATIA - V5" onClick={() => setToggle('CATIA - V5')}>CATIA - V5</ToggleButton>
-          }
+          <ToggleButton active={toggle === 'CATIA - V5'} onClick={() => setToggle('CATIA - V5')}>CATIA - V5</ToggleButton>
         </ToggleButtonGroup>
+
+        {/* Displaying Project Cards */}
         <CardContainer>
-          {toggle === 'all' && projects
-            .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal} />
-            ))}
           {projects
-            .filter((item) => item.category == toggle)
-            .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal} />
+            .filter((project) => toggle === 'all' || project.category === toggle)
+            .map((project, index) => (
+              <ProjectCard key={index} project={project} openModal={openModal} setOpenModal={setOpenModal} />
             ))}
         </CardContainer>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Projects
-
-
+export default Projects;
